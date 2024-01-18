970d64c5c7f7543394e100d7fa98a4adc6c8a749
Zachary Elbring <elbringzach@gmail.com>
2024-01-18 14:57:48 -0600

    Attempt to fix heroku deployment.

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', api);
 app.use(express.static('public'));
+app.use('/api', api);
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
