37dfd6db5bddf06d6309c3f9f4a36f3d07e625d7
Zachary Elbring <elbringzach@gmail.com>
2024-01-18 18:07:04 -0600

    Update server.js.

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use('/api', api);
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'))
 });
 
 app.listen(PORT, () => {
